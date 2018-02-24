039a38c2ef900533d8e102281fc6b5be324eb81d
renan.bfsilva <renan.bfsilva@CAS555494W1025.senacsp.edu.br>
2018-02-23 21:46:43 -0300

    Loop

--- agenda/src/main/java/br/senac/tads/pi3a/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3a/agenda/Agenda.java
@@ -93,7 +93,10 @@ public class Agenda {
         } catch (ParseException ex) {
             Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Contador " + i);
+        }
     }
 
 }
